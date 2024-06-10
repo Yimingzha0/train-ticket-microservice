867c8a484b47140b96538d593fda38e7007b733b
yiming <843889894@qq.com>
2024-06-10 11:56:23 +0200

    modify the bugs in rebook, route-plan, security, travel-plan

--- ts-rebook-service/src/main/java/rebook/RebookApplication.java
+++ ts-rebook-service/src/main/java/rebook/RebookApplication.java
@@ -20,7 +20,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAsync
 @IntegrationComponentScan
 @EnableSwagger2
-@EnableDiscoveryClient
 public class RebookApplication {
 
     public static void main(String[] args) {
